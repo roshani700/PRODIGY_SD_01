ec2a88155cc98b5fb7bbbcf37d7271c28072a6b4
roshani700 <143990513+roshani700@users.noreply.github.com>
2024-06-22 20:45:58 +0530

    Update TemperatureConversion.java

--- TemperatureConversion.java
+++ TemperatureConversion.java
@@ -13,7 +13,7 @@ public class TemperatureConversion{
 
     public static void convertTemperatureUnit(double t1){
         System.out.println("Temerature in celsius : "+t1+" °C");
-        System.out.println("Temperature in Fahrenheit : "+((t1*9/5)+32)+" °F");
+        System.out.println("Temperature in Fahrenheit : "+((t1*9.0/5.0)+32)+" °F");
         System.out.println("Temperature in Kelvin : "+ (t1+ 273.15)+" K");
 
     }
